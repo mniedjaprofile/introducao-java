0991e66c1cc5d3074e50958720114f04d81dd4f7
mniedjaprofile <167148695+mniedjaprofile@users.noreply.github.com>
2025-05-12 08:35:05 +0000

    Adicionando atividades do TP1

---
+++ TP1/App2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Pedro Henrique Cardoso";
+        int idade = 36;
+        double salario = 25.589;
+
+        System.out.println("O funcionário: " + nome + " " + "tem " + idade + " anos");
+        System.out.println("E recebe um salário de R$" + salario);
+    }
+}
