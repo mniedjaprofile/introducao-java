e6f13f32c1961a841eee73575bb1954196a74148
mniedjaprofile <167148695+mniedjaprofile@users.noreply.github.com>
2025-05-12 06:08:56 -0300

    Update Main.java

--- TP1/App3/src/Main.java
+++ TP1/App3/src/Main.java
@@ -1,19 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        dadosEntrada();
+       
     }
 
-    public static void dadosEntrada(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o seu nome: ");
-        String nome = sc.nextLine();
-
-        Scanner scs = new Scanner(System.in);
-        System.out.println("Digite a sua idade: ");
-        int idade = scs.nextInt();
-
-        System.out.println("Nome: " +nome +", Idade: " + idade);
-    }
 }
