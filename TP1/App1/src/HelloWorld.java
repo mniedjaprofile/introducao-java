9894e8179c1a0fbc951243f9a63bdeb410abd300
mniedjaprofile <167148695+mniedjaprofile@users.noreply.github.com>
2025-05-12 05:54:03 -0300

    Update HelloWorld.java

--- TP1/App1/src/HelloWorld.java
+++ TP1/App1/src/HelloWorld.java
@@ -1,18 +1,5 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class HelloWorld {
     public static void main(String[] args) {
 
-
-        LocalDate dataAtual = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataHoje = dataAtual.format(formatter);
-
-        System.out.println("============================================!");
-        System.out.println("============== Hello, World! ===============");
-        System.out.println("============================================!");
-        System.out.println("Olá meu nome é Mayara!");
-        System.out.println("Estou fazendo meu TP1 de JAVA hoje: " +dataHoje);
     }
 }
