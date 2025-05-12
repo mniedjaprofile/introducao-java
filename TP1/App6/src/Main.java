43dcfa1f20f4ea8ae6cd09aec8a0b0a16e690441
mniedjaprofile <167148695+mniedjaprofile@users.noreply.github.com>
2025-05-12 06:29:35 -0300

    Add files via upload

---
+++ TP1/App6/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 25;
+        int num2 = 20;
+
+        //Nome do metodo e variável
+        int soma = somar(num1, num2);
+        System.out.println(soma);
+    }
+
+
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+}
